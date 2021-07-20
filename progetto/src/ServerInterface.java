f4af5a4e15e3d1a42e19fda52a602d148a238fb3
Samuel <samuelpedotti92@gmail.com>
2021-07-20 15:23:02 +0200

    adding server

--- progetto/src/ServerInterface.java
+++ progetto/src/ServerInterface.java
@@ -1,2 +1,7 @@
-public interface ServerInterface {
+import java.io.IOException;
+import java.rmi.Remote;
+
+public interface ServerInterface extends Remote {
+    public static final int PORT = 8888;
+    public String echo(String string) throws IOException;
 }
--- progetto/src/ServerProxy.java
+++ progetto/src/ServerProxy.java
@@ -1,2 +1,30 @@
-public class ServerProxy {
+import java.net.*;
+import java.io.*;
+
+class ServerProxy implements ServerInterface {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ServerProxy() throws Exception {
+        InetAddress addr = InetAddress.getByName(null);
+        System.out.println("addr = " + addr);
+
+        socket = new Socket(addr, ServerInterface.PORT);
+        System.out.println("socket = " + socket);
+
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+    }
+
+    public String echo(String string) throws IOException {
+        return null;
+    }
+
+    public void close() throws IOException {
+        System.out.println("closing");
+        out.println("<end>");
+        socket.close();
+    }
 }
+
