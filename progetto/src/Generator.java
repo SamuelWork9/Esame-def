8fa78aca42bfc966c9b118725e759e1974ca1b7b
Samuel <samuelpedotti92@gmail.com>
2021-07-20 15:22:07 +0200

    adding generator

--- progetto/src/Generator.java
+++ progetto/src/Generator.java
@@ -1,2 +1,13 @@
 public class Generator {
+    private static int KEY = 0;
+    private String message;
+
+    public Generator(String nome) {
+        message = nome + "_" + (++KEY);
+        System.out.println("generated message: " + message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
