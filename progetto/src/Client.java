79e1f9fec89827176c7daeab689f7919e784f405
Samuel <samuelpedotti92@gmail.com>
2021-07-21 10:33:41 +0200

    adding correct client(no changes)

--- progetto/src/Client.java
+++ progetto/src/Client.java
@@ -1,30 +1,73 @@
-public class Client {
-    private static int ID = 0;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client extends MsgClient implements Serializable {
+    private final int PORTCLIENT = 8889;
     private String name;
     private String message;
 
-    public Client(String name) {
-        this.name = name + (++ID);
+    public Client() {
+        Random gen = new Random();
+        int a = gen.nextInt(1000);
+        this.name = "Cliente_" + a;
+        System.out.println("io sono il " + name);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setMessage(String message) {
+    /*public void setMessage(String message) {
         this.message = message;
-    }
+    }*/
 
-    public String getMessage() {
-        return message;
-    }
+    public String getName() { return this.name; }
 
     public static void main(String[] args) {
-        Client client = new Client("Client");
-        System.out.println(client.getName());
+        Socket sockCli;
+        try {
+            Client cli = new Client();
+            InetAddress addr = InetAddress.getByName(null);
+            System.err.println("Registry waiting");
+            sockCli = new Socket(addr, Registry.PORT); //connection required
 
-        Generatore gen = new Generatore(client.getName());
-        client.setMessage(gen.getMessage());
-        System.out.println(client.getMessage());
+            System.out.println("Mi sono collegato al registry");
+
+            RegistryData dataClient = new RegistryData();
+            dataClient.setStr("ECHO SERVICE");
+
+            ObjectOutputStream out = new ObjectOutputStream(sockCli.getOutputStream());
+            out.writeObject(dataClient);
+            // end request
+
+            System.out.println("Ritorno da registry");
+
+            ObjectInputStream in = new ObjectInputStream(sockCli.getInputStream());
+            dataClient = (RegistryData) in.readObject();
+
+            System.out.println("Ip server: " + dataClient.getIpServer());
+            System.out.println("Port server: " + dataClient.getPortServer());
+            System.out.println("Hashtable key: " + dataClient.getStr());
+
+            sockCli = new Socket(addr, dataClient.getPortServer()); //connection required
+            System.out.println("Mi sono collegato al server");
+
+            System.out.println("ora mi aggiungo alla lista dei client!");
+            ServerInterface server = (ServerInterface)dataClient.getObject();
+
+            MsgClient MsgC = new MsgClient(cli.getName());
+
+            while (true){
+                MsgC.generaMessaggio();
+                out = new ObjectOutputStream(sockCli.getOutputStream());
+                out.writeObject(MsgC);
+
+                System.out.println("Messaggio spedito al server!");
+                //ritorno
+                in = new ObjectInputStream(sockCli.getInputStream());
+                MsgC = (MsgClient) in.readObject();
+                System.out.println("messaggio dal server: " + MsgC.getEchoMessaggio());
+                Thread.sleep(10000);
+            }
+
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
+
