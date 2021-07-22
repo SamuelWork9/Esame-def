2b81d59dc2b51a28abf801c2ab0d01639fa143f3
Lorenzo <ldangelo1@studenti.uninsubria.it>
2021-07-22 04:22:25 +0200

    Ottimizzato Client
    numero univoco non funzionante

--- progetto/src/Client.java
+++ progetto/src/Client.java
@@ -1,36 +1,38 @@
 import java.io.Serializable;
 import java.net.InetAddress;
-import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.System.out;
 
 public class Client implements Serializable {
-    private static final int PORTCLIENT = 8888;
-    private String name;
-
-    public Client() {
-        Random gen = new Random();
-        int a = gen.nextInt(1000);
-        this.name = "Client_" + a;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public static void main(String[] args) {
-        try {
-            Client cli = new Client();
-            System.out.println(cli.getName());
-            InetAddress addr = InetAddress.getByName(null);
-            Proxy proxy = new Proxy(PORTCLIENT, addr);   //PORTCLIENT AL POSTO DI 8888.
-
-            System.out.println("Mi sono collegato al registry");
-            System.out.println("Mi sono collegato al server");
-            System.out.println("ora mi aggiungo alla lista dei client!");
-
-            proxy.exchangeMessage(cli);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
+	private static final int RegistryPORT_Client = 8888;
+	private static AtomicInteger count = new AtomicInteger(0);
+	private final String name;
+	
+	public Client() { // TODO non funziona il sequenziale, tutti client1
+		name = "Client" + count.incrementAndGet();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Client client = new Client();
+			out.println(client.getName());
+			
+			InetAddress address = InetAddress.getByName(null);
+			Proxy proxy = new Proxy(RegistryPORT_Client, address);
+			
+			out.println("I connected to the REGISTRY");
+			out.println("I connected to the SERVER");
+			out.println("I add myself to the client list!");
+			out.println();
+			
+			proxy.exchangeMessage(client);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
