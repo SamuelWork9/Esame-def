68c1f605334abbe7ee973d9612116fc750e198f4
Lorenzo <ldangelo1@studenti.uninsubria.it>
2021-07-22 04:19:47 +0200

    Ottimizzazione ServerThread
    aggiunto TODO per chiedere verifica

--- progetto/src/ServerThread.java
+++ progetto/src/ServerThread.java
@@ -1,41 +1,44 @@
 import java.io.*;
 import java.net.Socket;
 
+import static java.lang.System.out;
+
 public class ServerThread extends Thread implements ServerInterface {
-    private  Server srv;
-    private Socket sock;
-
-    public ServerThread(Server srv, Socket sock) {
-        this.srv = srv;
-        this.sock = sock;
-    }
-
-    public synchronized void echo(String msg, Socket sock) throws IOException {
-        System.out.println(Thread.currentThread() + ": Server: ha ricevuto " + msg);
-        try {
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
-            out.write(msg + "\n");
-            out.newLine();
-            out.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    @Override
-    public void run() {
-        System.err.println("I'm running");
-        BufferedReader in = null;
-        String MsgC = null;
-        while (true) {
-            try {
-                in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                MsgC = in.readLine();
-                System.out.println("messaggio ricevuto dal server: " + MsgC);
-                echo(MsgC, sock);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Echo eseguito");
-        }
-    }
+	private final Socket sock;
+	
+	public ServerThread(Socket sock) {
+		this.sock = sock;
+	}
+	
+	public synchronized void echo(String msg, Socket sock) throws IOException {
+		out.println(Thread.currentThread().getName() + " | server has received: " + msg); // todo era senza getname
+		try {
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+			out.write(msg + "\n");
+			out.newLine();
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		String MsgC;
+		BufferedReader in;
+		
+		System.err.println(", i'm running");
+		while (true) {
+			try {
+				in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				MsgC = in.readLine();
+				
+				out.println("Message received from the client: " + MsgC); //todo era messaggio from server
+				echo(MsgC, sock);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			out.println("echo performed");
+			out.println();
+		}
+	}
 }
