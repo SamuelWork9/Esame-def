2efe525b0efbaa11ee5356b45ee66dd466767c2c
Lorenzo <ldangelo1@studenti.uninsubria.it>
2021-07-22 04:22:38 +0200

    Ottimizzato Proxy

--- progetto/src/Proxy.java
+++ progetto/src/Proxy.java
@@ -3,57 +3,55 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Random;
 
+import static java.lang.System.out;
+
 public class Proxy {
-    private int PORTCLIENT;
-    private InetAddress addr;
-    private Socket sock;
-    private ObjectOutputStream out;
-    private ObjectInputStream in;
-    private RegistryData dataClient;
-    private Socket serverSocket;
-
-    public Proxy(int PORTCLIENT, InetAddress addr) throws IOException, ClassNotFoundException {
-        this.PORTCLIENT = PORTCLIENT;
-        this.addr = addr;
-        try {
-            this.sock = new Socket(this.addr, this.PORTCLIENT);
-            System.out.println(this.sock.toString());
-            dataClient = new RegistryData();
-            dataClient.setStr("ECHO SERVICE");
-
-            this.serverSocket = new Socket(addr, lookup(addr, PORTCLIENT).getPortServer());
-            System.out.println(this.serverSocket.toString());
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
-    public RegistryData lookup(InetAddress addr, int PORTCLIENT ) throws IOException, ClassNotFoundException {
-        out = new ObjectOutputStream(sock.getOutputStream());
-        out.writeObject(dataClient);
-        in = new ObjectInputStream(sock.getInputStream());
-        dataClient = (RegistryData) in.readObject();
-        return dataClient;
-    }
-
-    public synchronized void exchangeMessage(Client cli) throws InterruptedException, IOException {
-        Random gen = new Random();
-        while (true){
-            String MsgC = cli.getName() + gen.nextInt(100) + "\n";
-            System.out.println("Stringa: " + MsgC);
-
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
-            out.write(MsgC);
-            out.newLine();
-            out.flush();
-
-            System.out.println("Messaggio spedito al server! messaggio: " + MsgC);
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-            MsgC = in.readLine();
-            System.out.println("messaggio dal server: " + MsgC);
-            Thread.sleep(5000);
-        }
-    }
+	private Socket sock;
+	private RegistryData dataClient;
+	private Socket serverSocket;
+	
+	public Proxy(int RegistryPORT_Client, InetAddress address) {
+		try {
+			this.sock = new Socket(address, RegistryPORT_Client);
+			out.println(this.sock);
+			dataClient = new RegistryData();
+			dataClient.setStr("ECHO SERVICE");
+			
+			this.serverSocket = new Socket(address, lookup().getPortServer());
+			out.println(this.serverSocket);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public RegistryData lookup() throws IOException, ClassNotFoundException {
+		ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
+		out.writeObject(dataClient);
+		ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
+		dataClient = (RegistryData) in.readObject();
+		return dataClient;
+	}
+	
+	public synchronized void exchangeMessage(Client client) throws InterruptedException, IOException {
+		Random gen = new Random();
+		while (true) {
+			String strClient = client.getName() + "_" + gen.nextInt(1000);
+			out.println("Message generated: " + strClient);
+			
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
+			out.write(strClient);
+			out.newLine();
+			out.flush();
+			
+			System.out.println("Message sends to server: " + strClient);
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			strClient = in.readLine();
+			
+			System.out.println("Message from server: " + strClient);
+			System.out.println();
+			Thread.sleep(5000);
+		}
+	}
 }
-
